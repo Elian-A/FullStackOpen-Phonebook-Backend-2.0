a960765c53e034665e50cdd27f48467068ea4b0d
Elian Avalos <elianemanuelstudy@gmail.com>
2022-10-30 23:21:32 -0300

    Part 3 - Phonebook step 2

--- index.js
+++ index.js
@@ -6,4 +6,9 @@ app.get("/api/persons", (req, res) => {
   res.json(persons)
 })
 
+app.get("/info", (req, res) => {
+  const info = `Phonebook has info for ${persons.length} people\n ${new Date()}`
+  res.send(info)
+})
+
 app.listen(3001)
