87c6705295674a05f22cb032afbe510dfcafb9f7
Elian Avalos <elianemanuelstudy@gmail.com>
2022-10-30 23:47:33 -0300

    Part 3 - Phonebook step 4

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.get("/api/persons/:id", (req, res) => {
   res.json(person)
 })
 
+app.delete("/api/persons/:id", (req, res) => {
+  const id = +(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (!person) return res.status(404).end()
+  persons = persons.filter(person => person.id != id)
+  res.status(204).end()
+})
+
 app.get("/info", (req, res) => {
   const info = `Phonebook has info for ${persons.length} people\n ${new Date()}`
   res.send(info)
