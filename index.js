9bf2c50391885ccd53baccbad0146556779cf23b
Elian Avalos <elianemanuelstudy@gmail.com>
2022-11-02 09:16:37 -0300

    Part 3c - Phonebook Database step 3 and 4

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const app = express()
 app.use(express.json())
 app.use(express.static('dist'))
 app.use(cors())
-
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -15,27 +14,27 @@ app.get("/api/persons", (_, res) => {
   Person.find({}).then(persons => res.json(persons))
 })
 
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res, next) => {
   const { name, number } = req.body
 
   if (!name || !number) return res.status(400).send({ error: `name and number are required` })
   // Need to Validate if a person already exist, need more knowledge
   // Person.find({ name }).then(person => res.status(400).send({ error: `name must be unique` }))
-  const newPerson = new Person({name, number})
-  newPerson.save().then(person => res.status(201).json(person)).catch(err => console.error(err.message))
+  const newPerson = new Person({ name, number })
+  newPerson.save().then(person => res.status(201).json(person)).catch(err => next(err))
 })
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
   const id = req.params.id
-  Person.findById(id).then(person => res.json(person)).catch(err => res.status(404).end())
+  Person.findById(id).then(person => res.json(person)).catch(err => next(err))
 })
 
-app.delete("/api/persons/:id", (req, res) => {
+app.delete("/api/persons/:id", (req, res, next) => {
   const id = req.params.id
   Person.findByIdAndDelete(id).then(deletedObject => {
     if (!deletedObject) return res.status(404).end()
     res.status(204).end()
-  }).catch(err => console.error(err))
+  }).catch(err => next(err))
 })
 
 app.get("/info", (req, res) => {
@@ -45,6 +44,15 @@ app.get("/info", (req, res) => {
 
 const unknownEndpoint = (req, res) => res.status(404).send({ error: "Unknow endpoint" })
 app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  if(error.name === 'CastError') {
+    console.error("Error Message: ", error.message);
+    res.status(400).send({error: 'Invalid Id'})
+  }
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = 3001
 app.listen(PORT)
