7f360ad5954ec4a525d19d37827e640b7befc979
Elian Avalos <elianemanuelstudy@gmail.com>
2022-11-03 08:28:40 -0300

    Part 3d - Phonebook Database step 7 and 8 Part 3d - Eslint config

--- index.js
+++ index.js
@@ -1,60 +1,62 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./mongo')
+
 const app = express()
 
 app.use(express.json())
 app.use(express.static('dist'))
 app.use(cors())
-morgan.token('body', (req, res) => JSON.stringify(req.body))
+morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.get("/api/persons", (_, res) => {
-  Person.find({}).then(persons => res.json(persons))
+app.get('/api/persons', (_, res) => {
+  Person.find({}).then((persons) => res.json(persons))
 })
 
-app.post("/api/persons", (req, res, next) => {
+app.post('/api/persons', (req, res, next) => {
   const { name, number } = req.body
 
-  if (!name || !number) return res.status(400).send({ error: `name and number are required` })
+  if (!name || !number) return res.status(400).send({ error: 'name and number are required' })
   const newPerson = new Person({ name, number })
-  newPerson.save().then(person => res.status(201).json(person)).catch(err => next(err))
+  newPerson.save().then((person) => res.status(201).json(person)).catch((err) => next(err))
 })
 
-app.get("/api/persons/:id", (req, res, next) => {
-  const id = req.params.id
-  Person.findById(id).then(person => res.json(person)).catch(err => next(err))
+app.get('/api/persons/:id', (req, res, next) => {
+  const { id } = req.params
+  Person.findById(id).then((person) => res.json(person)).catch((err) => next(err))
 })
 
-app.delete("/api/persons/:id", (req, res, next) => {
-  const id = req.params.id
-  Person.findByIdAndDelete(id).then(deletedObject => {
+app.delete('/api/persons/:id', (req, res, next) => {
+  const { id } = req.params
+  Person.findByIdAndDelete(id).then((deletedObject) => {
     if (!deletedObject) return res.status(404).end()
     res.status(204).end()
-  }).catch(err => next(err))
+  }).catch((err) => next(err))
 })
 
-app.put("/api/persons/:id", (req, res, next) => {
-  const id = req.params.id
+app.put('/api/persons/:id', (req, res, next) => {
+  const { id } = req.params
   const { name, number } = req.body
-  if (!name || !number) return res.status(400).send({ error: `name and number are required` })
+  if (!name || !number) return res.status(400).send({ error: 'name and number are required' })
   const updatedPerson = {
     name, number
   }
-  Person.findByIdAndUpdate(id, updatedPerson, { new: true, runValidators: true }).then(updated => {
-    res.send(updated)
-  }).catch(err => next(err))
+  Person.findByIdAndUpdate(id, updatedPerson, { new: true, runValidators: true })
+    .then((updated) => res.send(updated))
+    .catch((err) => next(err))
 })
 
-app.get("/info", (req, res) => {
+app.get('/info', (req, res) => {
   Person.find({})
-    .then(persons => {
+    .then((persons) => {
       res.send(`Phonebook has info for ${persons.length} people\n ${new Date()}`)
     })
 })
 
-const unknownEndpoint = (req, res) => res.status(404).send({ error: "Unknow endpoint" })
+const unknownEndpoint = (req, res) => res.status(404).send({ error: 'Unknow endpoint' })
 app.use(unknownEndpoint)
 
 const errorHandler = (error, req, res, next) => {
@@ -68,6 +70,6 @@ const errorHandler = (error, req, res, next) => {
 }
 app.use(errorHandler)
 
-const PORT = 3001
+const { PORT } = process.env
 app.listen(PORT)
 console.log(`App running on port ${PORT}`)
--- mongo.js
+++ mongo.js
@@ -4,14 +4,16 @@ const uniqueValidator = require('mongoose-unique-validator')
 
 const numberValidator = (number) => {
   let counter = 0
-  for (let char of number) if (/\d/.test(char)) counter++
+  Array(number).forEach((char) => {
+    if (/\d/.test(char)) counter += 1
+  })
   if (counter >= 8) return true
   return false
 }
 
 const uri = process.env.MONGO_URI
-mongoose.connect(uri).then(res => console.log('Connected'))
-  .catch(err => console.error(`Error: ${err.message}`))
+mongoose.connect(uri).then(() => console.log('Connected'))
+  .catch((err) => console.error(`Error: ${err.message}`))
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -24,7 +26,7 @@ const personSchema = new mongoose.Schema({
     type: String,
     validate: {
       validator: numberValidator,
-      message: props => `${props.value} is not a valid number`
+      message: (props) => `${props.value} is not a valid number`
     },
     unique: true,
     required: true
